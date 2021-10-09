196b6a2a4706631d4066221394f1430e1127efd2
Rezocrypt <87856097+Rezocrypt@users.noreply.github.com>
2021-10-09 09:40:53 +0400

    Add files via upload

---
+++ Fizmat_Bot/Files/Weather/main.py
@@ -0,0 +1,19 @@
+# import the module
+import python_weather
+import asyncio
+
+async def getweather():
+    # declare the client. format defaults to metric system (celcius, km/h, etc.)
+    client = python_weather.Client(format=python_weather.IMPERIAL)
+
+    # fetch a weather fore
+
+    cast from a city
+    weather = await client.find("Yerevan AM")
+    print(weather)
+    # close the wrapper once done
+    await client.close()
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(getweather())
